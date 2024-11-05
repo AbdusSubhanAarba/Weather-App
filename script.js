71a66557ceb24f9b78f0a695984c78c6038ecf69
Abdus Subhan Aarba <abdusubhan7@gmail.com>
2024-11-05 19:31:28 +0300

    script.js

--- script.js
+++ script.js
@@ -21,17 +21,17 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/clouds.png";
     } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/clear.png";
     } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/rain.png";
     } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/drizzle.png";
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/mist.png";
     } else if (data.weather[0].main == "Snow") {
-      weatherIcon.src = "images/snow.png";
+      weatherIcon.src = "https://raw.githubusercontent.com/AbdusSubhanAarba/Weather-App/main/snow.png";
     }
 
     document.querySelector(".weather").style.display = "block";
